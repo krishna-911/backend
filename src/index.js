796a8f90648593726efbf3a74a67adc985b7d595
Dwitikrishna Parida <krishnaparida011@gmail.com>
2025-02-01 00:52:58 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ dotenv.config({
 
 
 connectDB()
+
 .then(() => {
     app.listen(process.env.PORT || 3000, () => {
         console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,8 +3,8 @@ import { userSignup, userSignin } from "../controllers/user.controller.js"
 
 const router = Router();
 
-Router.route("/signup").post(userSignup);
+router.route("/signup").post(userSignup);
 
-Router.route("/signin").post(userSignin);
+router.route("/signin").post(userSignin);
 
 export default router
